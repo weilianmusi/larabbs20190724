58d353e993c48376d92bc23b73b582b61b46adbd
vagrant <vagrant@homestead>
2019-07-26 06:36:56 +0000

    users topic show

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -16,4 +16,32 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 按照 updated_at 排序
+        return $query->orderBy('updated_at', 'desc');
+    }
 }
